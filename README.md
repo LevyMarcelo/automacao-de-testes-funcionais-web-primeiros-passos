97d8057adef3fbd6700fce4d1cfc27619d3b7ead
LevyMarcelo <lmgcunha@restinga.ifrs.edu.br>
2019-06-03 10:39:58 -0300

    automacao-de-testes-funcionais-web-primeiros-passos

---
+++ README.md
@@ -0,0 +1,11 @@
+# Automação de Testes Funcionais Web - Primeiros Passos
+
+As ferramentas utilizadas são:
+
+	• Selenium WebDriver 
+	• JUnit
+	• Maven 
+	• jdk 1.8.0
+	
+	
+
---
+++ src/test/java/tests/PesquisaGoogleTest.java
@@ -0,0 +1,40 @@
+package tests;
+
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PesquisaGoogleTest {
+
+    private static WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\levy.cunha\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://www.google.com.br");
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void pesquisaGoogle() {
+        // 1° forma
+        WebElement campoPesquisa = driver.findElement(By.name("q"));
+        campoPesquisa.sendKeys("SoftDesign");
+
+        // 2° forma
+        // driver.findElement(By.name("q")).sendKeys("SoftDesign");
+
+        WebElement botaoPesquisa = driver.findElement(By.name("btnK"));
+        botaoPesquisa.click();
+
+        Assert.assertEquals(driver.findElement(By.className("r"))
+                .getText(), "SoftDesign: Desenvolvimento de Software, Apps, Sistemas e ...");
+    }
+}
